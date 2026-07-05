c4906f3c373e885595872d4a71ba0ccce15fa3a1
Priyansh Gupta <priyansh@Priyanshs-MacBook-Air.local>
2026-07-05 15:32:00 +0530

    fixes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,8 +2,7 @@ import { tabs } from "@/constants/data";
 import { colors, components } from "@/constants/theme";
 import { clsx } from "clsx";
 import { Tabs } from "expo-router";
-import { Image } from "react-native";
-import { View } from "react-native-css/components/View";
+import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const TabIcon = ({ focused, icon }: TabIconProps) => {
--- components/shared/safe-view.tsx
+++ components/shared/safe-view.tsx
@@ -12,7 +12,7 @@ type SafeViewProps = {
 
 const SafeView = ({ children, className = "" }: SafeViewProps) => {
   return (
-    <SafeAreaView className={twMerge(clsx("p-2", className))}>
+    <SafeAreaView className={twMerge(clsx("p-2 flex-1", className))}>
       {children}
     </SafeAreaView>
   );
